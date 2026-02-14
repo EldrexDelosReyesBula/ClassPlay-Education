a191be9da3caecfc8ccf5c0543a275365c80b688
Eldrex Delos Reyes Bula <eldrexdelosreyesbula@gmail.com>
2026-02-14 21:22:45 +0800

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        'react',
+        'react-dom',
+        'react-dom/client',
+        'react/jsx-runtime',
+        'read-excel-file',
+        'uuid',
+        'three',
+        '@react-three/fiber',
+        '@react-three/drei',
+        '@vercel/analytics/react'
+      ]
+    }
+  }
+});
